50d1a3e8e2c7bae910bbcc4fa98e93e69bf95f05
jurit <gothicrecordsllc@gmail.com>
2025-11-18 23:04:28 +0100

    Fix: Rimosso optimizeCss che richiede critters

--- next.config.mjs
+++ next.config.mjs
@@ -25,7 +25,6 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ['react-icons', 'framer-motion'],
     scrollRestoration: true,
-    optimizeCss: true,
   },
   
   compiler: {
